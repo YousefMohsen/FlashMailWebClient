6cb82eab384f5ced853366145e01f33e5c499c17
YousefMohsen <yousinho7@live.dk>
2017-10-20 00:02:06 +0200

    add new message form + axios

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MsgForm from './MsgForm'; 
+
+class App extends Component {
+  render() {
+    return (
+        <MsgForm/>
+    );
+  }
+}
+
+export default App;
